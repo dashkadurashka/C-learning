1af93a657ccd02c1b08ada4a86f342686ee7fa03
dashkadurashka <domand9475@gmail.com>
2023-02-27 22:46:22 +0300

    add Lesson 001 Condition If-Else

---
+++ Lesson001_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("We are glad to see you, Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}

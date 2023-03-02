c9e90f29547add5b4d89afca940d2aa8d11fa774
dashkadurashka <domand9475@gmail.com>
2023-03-02 11:53:40 +0300

    add sqwr2 task

---
+++ Lesson001_sqwr2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+Console.Write("Enter first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter first number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 * number2 == number1)
+{
+    Console.WriteLine("Yes");
+}
+
+else
+{
+    Console.WriteLine("No");
+}

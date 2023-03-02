81f9703622fe042b1530967b446ce7857d4c69ad
dashkadurashka <domand9475@gmail.com>
2023-03-02 11:41:28 +0300

    Homework for lesson 1

---
+++ Homework_Lesson001/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, \какое число большее, а какое меньшее.
+Console.Write("Enter first number: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+}
+
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+}
+
+
+
---
+++ Homework_Lesson001/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел
+
+Console.Write("Input first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input third nnmber: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+    if (c > max)
+    {
+        max = c;
+    }
+}
+
+Console.WriteLine(max);
+
+

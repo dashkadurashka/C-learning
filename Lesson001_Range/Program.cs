9c5eee6e8998e5017e4f2f0f19e0e0f81f1f6af0
dashkadurashka <domand9475@gmail.com>
2023-03-02 12:00:25 +0300

    add range task

---
+++ Lesson001_Range/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), 
+//а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = - number;
+
+while(i <= number)
+{
+    Console.Write(i);
+    Console.Write(",");
+    i++;
+}

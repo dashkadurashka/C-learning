c3345f93ce5dedb0791f587a11bb8a57b6a40775
dashkadurashka <domand9475@gmail.com>
2023-03-02 11:51:46 +0300

    add weekday task

---
+++ Lesson001_Week/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+Console.Write("Input number: ");
+int week = Convert.ToInt32(Console.ReadLine()); 
+
+switch (week) 
+{
+    case 1: 
+        Console.WriteLine("Monday");
+        break;
+    
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+
+    case 5:
+        Console.WriteLine("Friday");
+        break;
+
+    case 6:
+        Console.WriteLine("Saturday");
+        break;
+
+    case 7:
+        Console.WriteLine("Sanday");
+        break;
+
+    default:
+        Console.WriteLine("There is no sush a weekday");
+        break;
+    
+   
+}

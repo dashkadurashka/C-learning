cd6ff8454acb715d657acce16a8d1bd3e3c73979
dashkadurashka <domand9475@gmail.com>
2023-03-02 11:52:51 +0300

    add sqwr task

---
+++ Lesson001_sqwr/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Result: ");
+Console.WriteLine(number * number);
+

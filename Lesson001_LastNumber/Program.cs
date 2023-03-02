c2e2958ede7c34abedeb796cbc5e1e25fc814e4e
dashkadurashka <domand9475@gmail.com>
2023-03-02 12:01:16 +0300

    add last number task

---
+++ Lesson001_LastNumber/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает последнюю цифру этого числа.
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(number % 10);

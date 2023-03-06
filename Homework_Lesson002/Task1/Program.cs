2f015b7154d8751c0a2949fdaefb0a81238a30e1
dashkadurashka <domand9475@gmail.com>
2023-03-07 00:38:27 +0300

    Homework for lesson 2

---
+++ Homework_Lesson002/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    int secondNumber = number % 100 / 10;
+    Console.WriteLine($"Second number {secondNumber}");
+
+}
+else
+{
+    Console.WriteLine("Incorrect number");
+}
---
+++ Homework_Lesson002/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+Random weekDay = new Random();
+int number = weekDay.Next(1, 8);
+Console.WriteLine(number);
+
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("Yes");
+}
+else Console.WriteLine("No");

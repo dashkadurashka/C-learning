6d64d2641089490c9837f8de45ea6bfd7daa78d5
dashkadurashka <domand9475@gmail.com>
2023-03-20 00:38:08 +0300

    Homework for lesson 6

---
+++ Homework_Lesson006/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+int count = 0;
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+Console.WriteLine("Input 666 for exit");
+void GetCountOfNumber()
+{
+    while(true)
+    {
+        int number = Prompt("Input number: ");
+        if(number == 666) break;
+        else
+            if(number > 0) count += 1;
+    }
+}
+
+void PrintResult (int count)
+{
+    Console.WriteLine($"Чисел больше нуля - {count}");
+}
+
+GetCountOfNumber();
+PrintResult(count);
---
+++ Homework_Lesson006/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double PromptDouble(string message) 
+{
+    Console.Write(message);
+    double result = Convert.ToDouble(Console.ReadLine());
+    return result;
+}
+    
+double b1 = PromptDouble("Введите b1:");
+double k1 = PromptDouble("Введите k1:");
+double b2 = PromptDouble("Введите b2:");
+double k2 = PromptDouble("Введите k2:");
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Точка пересечения двух прямых: ({x}; {y}).");
